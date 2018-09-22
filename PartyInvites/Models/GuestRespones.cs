0d253755c1475337f57247e14dd453f5b4727ae5
Derrick Hubbard <derrickhubb@gmail.com>
2018-09-22 09:18:10 -0400

    update1

--- PartyInvites/Models/GuestRespones.cs
+++ PartyInvites/Models/GuestRespones.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestRespones
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
-        public bool? WillAttend { get; set; }
+        [Required(ErrorMessage = "Please specify whether you’ll attend")]
+        public bool? WillAttend { get; set; }
     }
 }
